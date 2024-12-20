0f4aa9e5b283b99d39d229e4a69352c3428e92e3
Znackt <Znackt@proton.me>
2024-12-20 21:14:41 +0530

    Added a test script

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Hero from "./components/Hero";
 
 const App = () => {
-  return <main className="relative min-h-screen w-screen overflow-x-hidden">
-    < Hero />
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
 
-    <section className="z-0 min-h-screen bg-blue-500"/>
-  </main>;
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
+  );
 };
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,16 +4,13 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button
       id={id}
-      className={
-        `group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black
-        ${containerClass}`
-    }>
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black
+        ${containerClass}`}
+    >
       {leftIcon}
 
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
-        <div>
-            {title}
-        </div>
+        <div>{title}</div>
       </span>
       {rightIcon}
     </button>
