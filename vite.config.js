b3bf7029d074d455155b0f71ab3bf8ced3be99b7
Znackt <Znackt@proton.me>
2025-01-09 19:59:06 +0530

    Chenged the config port to 8080

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()],
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 8080,
+  strictPort: true,
+ },
+ server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+ },
 });
