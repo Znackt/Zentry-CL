38b0279ed7828be32319b4adcd20ced1d1aebc26
Znackt <Znackt@proton.me>
2025-01-11 19:45:26 +0530

    added a padding in x for better spacing and all

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
-        <nav className="flex size-full items-center justify-between ">
+        <nav className="flex size-full items-center justify-between px-6">
           <div className="flex items-center gap-7">
             <img src="/img/logo.png" alt="logo" className="w-10" />
 
@@ -70,7 +70,7 @@ const Navbar = () => {
               id="product-button"
               title="Products"
               rightIcon={<TiLocationArrow />}
-              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1 "
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
             />
           </div>
 
