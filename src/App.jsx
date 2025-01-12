6dd16a44a247e8a14336023ff394c8d776829acb
Znackt <Znackt@proton.me>
2025-01-12 22:29:02 +0530

    Completed creating the story and started impl Contact and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <About />
-      <Features />
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
     </main>
   );
 };
